70ca427a27a4de7c6e721caa4c96f184ce74c89b
anushreerathore <rathoreanushree00>
2023-08-01 11:30:51 +0530

    message

---
+++ Rathore.java
@@ -0,0 +1,6 @@
+public class Rathore{ 
+public static void main (String[]args){
+    String name="anushree rathore";
+    System.out.print("my name is:"+name);
+}
+}
---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter first number = ");
+        int x=sc.nextInt();
+        System.out.print("enter second number = ");
+        int y=sc.nextInt();
+        System.out.println("sum = "+(x+y));
+    }
+}
